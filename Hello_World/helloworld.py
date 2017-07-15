29ac06eac5ac17a0d9320ff3f09801d604a34736
mbuntu <marshgauch@gmail.com>
2017-07-14 18:40:20 -0600

    Initial commit with Hello World app

---
+++ Hello_World/helloworld.py
@@ -0,0 +1,11 @@
+# Program: helloworld.py
+#
+# Date: 7/14/17
+#
+# Description: Good ol' Hello World 
+#
+def main():
+    print "{}".format("Hello World@!")
+
+if __name__ == "__main__":
+    main()
